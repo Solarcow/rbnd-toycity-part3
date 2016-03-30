507585f70733eafec3e74ad63ad08f188394b18d
Solarcow <Alex.Kamrath@live.com>
2016-03-31 09:51:56 +1000

    add new methods for products

--- lib/product.rb
+++ lib/product.rb
@@ -1,11 +1,36 @@
 class Product
+	attr_reader :title
+
   @@products = []
 
   def initialize(options={})
-    @@products << self
+  	@title = options[:title]
+  	@price = options[:price]
+  	@stock = options[:stock]
+    add_to_products
   end
 
   def self.all
     @@products
+  end
+
+  def find_by_title
+  	@title
+  end
+
+  def in_stock?
+  	if @stock > 0
+  		return true
+  	else
+  		return false
+  	end
+  end
+  private
+
+  def add_to_products(title)
+    # if @title == 
+    # 	raise DuplicateProductError, "#{@title} already exists!"
+    # else
+    @@products << self
   end
 end
