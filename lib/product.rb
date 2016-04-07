415d5c6b44773e7aeb436b67656b8a2fc06c5b20
Solarcow <Alex.Kamrath@live.com>
2016-04-07 18:47:16 +1000

    Add find and private methods in products

--- lib/product.rb
+++ lib/product.rb
@@ -1,5 +1,5 @@
 class Product
-	attr_reader :title
+	attr_accessor :title, :price, :stock
 
   @@products = []
 
@@ -14,10 +14,10 @@ class Product
     @@products
   end
 
-  def find_by_title
-  	@title
+  def self.find_by_title(title)
+    @@products.find{|product| product.title == title}
   end
-
+ 
   def in_stock?
   	if @stock > 0
   		return true
@@ -27,10 +27,26 @@ class Product
   end
   private
 
-  def add_to_products(title)
-    # if @title == 
-    # 	raise DuplicateProductError, "#{@title} already exists!"
-    # else
-    @@products << self
+  def add_to_products
+    @@products.each do |product| 
+      if product.title == @title
+        raise DuplicateProductError, "#{title} already exists"
+      end
+    end
+     @@products<< self
+    # unless @@products.include?(self.title)
+    #   @@products<< self
+    # end
   end
 end
+
+
+
+
+
+
+
+
+
+
+
